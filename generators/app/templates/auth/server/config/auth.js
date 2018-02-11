689948b03d594ec7d4f01775c132b809b3c6aea7
Amimaro <amir.zahlan@gmail.com>
2018-02-11 09:00:12 -0300

    Update callback env url

--- generators/app/templates/auth/server/config/auth.js
+++ generators/app/templates/auth/server/config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
   'facebookAuth': {
     'clientID': process.env.FACEBOOK_KEY,
     'clientSecret': process.env.FACEBOOK_SECRET,
-    'callbackURL': process.env.APP_URL + 'api/user/auth/facebook/callback',
+    'callbackURL': process.env.SYNC_URL + 'api/user/auth/facebook/callback',
     'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
     'profileFields': ['id', 'email', 'name']
   },
@@ -13,19 +13,19 @@ module.exports = {
   'twitterAuth': {
     'consumerKey': process.env.TWITTER_KEY,
     'consumerSecret': process.env.TWITTER_SECRET,
-    'callbackURL': process.env.APP_URL + 'api/user/auth/twitter/callback'
+    'callbackURL': process.env.SYNC_URL + 'api/user/auth/twitter/callback'
   },
 
   'googleAuth': {
     'clientID': process.env.GOOGLE_KEY,
     'clientSecret': process.env.GOOGLE_SECRET,
-    'callbackURL': process.env.APP_URL + 'api/user/auth/google/callback'
+    'callbackURL': process.env.SYNC_URL + 'api/user/auth/google/callback'
   },
 
   'githubAuth': {
     'clientID': process.env.GITHUB_KEY,
     'clientSecret': process.env.GITHUB_SECRET,
-    'callbackURL': process.env.APP_URL + 'api/user/auth/github/callback'
+    'callbackURL': process.env.SYNC_URL + 'api/user/auth/github/callback'
   }
 
 };
