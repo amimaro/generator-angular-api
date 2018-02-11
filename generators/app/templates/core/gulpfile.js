477259c26c12f0ced51ce5ae41d6920dfc9b27ca
Amimaro <amir.zahlan@gmail.com>
2018-02-11 08:59:26 -0300

    Update gulpfile.js

--- generators/app/templates/core/gulpfile.js
+++ generators/app/templates/core/gulpfile.js
@@ -1,13 +1,39 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var nodemon = require('gulp-nodemon');
 
-gulp.task('default', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./client/dist/"
-    },
+gulp.task('browser-sync', ['nodemon'], function() {
+  browserSync({
+    proxy: "localhost:8000/",
+    port: 8080,
     notify: false
   });
-  gulp.watch('client/dist/index.html').on("change", reload);
+});
+
+gulp.task('nodemon', function (cb) {
+  var called = false;
+  return nodemon({
+    script: 'server/index.js',
+    ignore: [
+      'gulpfile.js',
+      'client/',
+      'node_modules/'
+    ]
+  })
+  .on('start', function () {
+    if (!called) {
+      called = true;
+      cb();
+    }
+  })
+  .on('restart', function () {
+    setTimeout(function () {
+      reload({ stream: false });
+    }, 1000);
+  });
+});
+
+gulp.task('default', ['browser-sync'], function () {
+  gulp.watch(['client/dist/index.html'], reload);
 });
