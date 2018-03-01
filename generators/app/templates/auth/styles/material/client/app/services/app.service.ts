627f7ba35e33889299d028cad624f47ae89eeab2
Amimaro <amir.zahlan@gmail.com>
2018-03-01 20:58:41 -0300

    Add check if logged in function

--- generators/app/templates/auth/styles/material/client/app/services/app.service.ts
+++ generators/app/templates/auth/styles/material/client/app/services/app.service.ts
@@ -7,21 +7,35 @@ export class AppService {
 
   user: any;
   message: string = "";
-  apiUrl: string = 'http://localhost:8080/api/'
+  apiUrl: string = 'http://localhost:8080/api/';
+  isLoggedIn: boolean = false;
 
   constructor(
     private router: Router,
     private http: HttpClient
   ) { }
 
-  routeTo(route){
+  routeTo(route) {
     this.router.navigate(route);
   }
 
-  getUser () { return this.user; }
+  getUser() { return this.user; }
 
   getSession() {
     return this.http.get(this.apiUrl + 'user/auth/session');
+  }
+
+  getIsLoggedIn() {
+    this.getSession()
+      .subscribe(
+      res => {
+        this.isLoggedIn = true;
+        console.log(this.isLoggedIn)
+      },
+      err => {
+        this.isLoggedIn = false;
+        console.log(this.isLoggedIn)
+      });
   }
 
   loginFacebook() {
