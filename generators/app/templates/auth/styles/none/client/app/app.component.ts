9a417d3da0343aff4e07ce6807b6995ab2035c64
Amimaro <amir.zahlan@gmail.com>
2018-03-01 20:59:42 -0300

    Add service to auth app component

--- generators/app/templates/auth/styles/none/client/app/app.component.ts
+++ generators/app/templates/auth/styles/none/client/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AppService } from './services/app.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(public appService: AppService) { }
+  
 }
